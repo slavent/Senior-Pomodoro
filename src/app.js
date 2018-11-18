b79acfc37ad500fbdf2f7142bb45536f933d70f7
Slavent <startupne@gmail.com>
2018-11-18 15:24:58 +0300

    footer

--- src/app.js
+++ src/app.js
@@ -180,7 +180,14 @@ class App extends React.Component {
                             }
                         </Col>
                     </Row>
-                    <footer/>
+                    <footer>
+                        <hr/>
+                        <p>
+                            Created by <a href="mailto:startupne@gmail.com">Slavent</a>.
+                            <span> </span>
+                            { new Date().getYear() + 1900 }
+                        </p>
+                    </footer>
                 </Container>
             </div>
         )
