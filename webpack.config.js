f1a9cc3a6ad6e6f6078f7d773fcad74906bd306e
Slavent <startupne@gmail.com>
2018-11-18 13:26:36 +0300

    add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,15 @@ module.exports = {
                     fallback: "style-loader",
                     use: [ "css-loader" ]
                 } )
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {}
+                    }
+                ]
             }
         ]
     },
