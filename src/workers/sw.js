3d8d064558b7e823066385462765afd442286ec5
Slavent <startupne@gmail.com>
2019-03-25 15:59:08 +0300

    cache api requests

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -5,3 +5,19 @@ if ( workbox ) {
 } else {
     console.log( `Boo! Workbox didn't load 😬` )
 }
+
+workbox.routing.registerRoute(
+    /.*api.*/,
+    new workbox.strategies.NetworkFirst( {
+        cacheName: "api-pomodoro",
+        plugins: [
+            new workbox.cacheableResponse.Plugin( {
+                statuses: [0, 200]
+            } ),
+            new workbox.expiration.Plugin( {
+                maxAgeSeconds: 60 * 60 * 24,
+                maxEntries: 30
+            } )
+        ]
+    } )
+)
