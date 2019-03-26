1a4f66895bca506ca925b773c28b5387c78351d5
Slavent <startupne@gmail.com>
2019-03-26 15:29:56 +0300

    lock btn

--- src/containers/Home/AddTaskForm/index.js
+++ src/containers/Home/AddTaskForm/index.js
@@ -6,53 +6,59 @@ import { onCreateTask } from "middlewares"
 import { onInputEstimate, onInputPriority, onInputTitle } from "actions"
 import "./style.css"
 
-const AddTaskForm = ( { title, estimate, priority, onInputEstimate, onInputPriority, onInputTitle, onCreateTask } ) =>
-    <div>
-        <Row>
-            <Col xs={ 12 }>
-                <div className="addform">
-                    <div className="addform__item">
-                        <Input
-                            style={ { height: 100 } }
-                            id="input"
-                            type="textarea"
-                            value={ title }
-                            placeholder="Buy tomatoes..."
-                            onChange={ onInputTitle }/>
+const AddTaskForm = ( { title, estimate, priority, onInputEstimate, onInputPriority, onInputTitle, onCreateTask } ) => {
+    const isButtonLock = !title || !priority || !estimate
+
+    return (
+        <div>
+            <Row>
+                <Col xs={ 12 }>
+                    <div className="addform">
+                        <div className="addform__item">
+                            <Input
+                                style={ { height: 100 } }
+                                id="input"
+                                type="textarea"
+                                value={ title }
+                                placeholder="Buy tomatoes..."
+                                onChange={ onInputTitle }/>
+                        </div>
+                        <div className="addform__item">
+                            <Input
+                                type="select"
+                                value={ priority }
+                                onChange={ onInputPriority }>
+                                <option>{ PRIORITIES.MINOR }</option>
+                                <option>{ PRIORITIES.MAJOR }</option>
+                                <option>{ PRIORITIES.CRITICAL }</option>
+                            </Input>
+                        </div>
+                        <div className="addform__item">
+                            <Input
+                                type="select"
+                                value={ estimate }
+                                onChange={ onInputEstimate }>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Input>
+                        </div>
                     </div>
-                    <div className="addform__item">
-                        <Input
-                            type="select"
-                            value={ priority }
-                            onChange={ onInputPriority }>
-                            <option>{ PRIORITIES.MINOR }</option>
-                            <option>{ PRIORITIES.MAJOR }</option>
-                            <option>{ PRIORITIES.CRITICAL }</option>
-                        </Input>
-                    </div>
-                    <div className="addform__item">
-                        <Input
-                            type="select"
-                            value={ estimate }
-                            onChange={ onInputEstimate }>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Input>
-                    </div>
-                </div>
-            </Col>
-        </Row>
-        <div className="addform__wrapper">
-            <button
-                className="addform__button"
-                onClick={ onCreateTask }>
-                Add task
-            </button>
+                </Col>
+            </Row>
+            <div className="addform__wrapper">
+                <button
+                    disabled={ isButtonLock }
+                    className={ "addform__button " + ( isButtonLock ? "addform__button-disabled" : "" ) }
+                    onClick={ onCreateTask }>
+                    Add task
+                </button>
+            </div>
         </div>
-    </div>
+    )
+}
 
 const mapStateToProps = state => state
 const actions = {
