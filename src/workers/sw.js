575d494835cdcb1ea2d76a76029e2785915af038
Slavent <startupne@gmail.com>
2019-03-27 15:11:47 +0300

    resend failed post request

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -9,3 +9,28 @@ workbox.routing.registerRoute( /api/, new workbox.strategies.NetworkFirst( {
     ]
 } ), "GET" )
 
+workbox.routing.registerRoute(
+    /api/,
+    async( { url, event, params } ) => {
+        const { request } = event
+
+        return fetch( request.clone() )
+            .then( response => response )
+            .catch( error => onFail( request ) )
+    },
+    "POST"
+)
+
+const onFail = request => {
+    const intervalId = setInterval( () => {
+        console.log( "Connection is " + ( navigator.onLine ? "success" : "fail." ) )
+
+        if ( navigator.onLine ) {
+            fetch( request )
+                .then( response => response )
+                .catch( error => console.error( error ) )
+
+            clearInterval( intervalId )
+        }
+    }, 1000 )
+}
