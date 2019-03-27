bba1e7ad906fc19f130ab32785d80bfdca07c9bb
Slavent <startupne@gmail.com>
2019-03-27 11:05:36 +0300

    fix logo link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import Menu from "components/Menu"
 import "./style.css"
+import { Link } from "react-router-dom"
 
 const Header = () =>
     <header>
         <h2>
-            <a href="/">
+            <Link to={ "/" }>
                 <div className="logo"/>
                 Senior Pomodoro
-            </a>
+            </Link>
         </h2>
         <Menu/>
     </header>
